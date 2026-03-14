1d3f4d66faebe3629f77a8cc9bb0428f357f3d74
vietnamexam <vanhayd2@gmail.com>
2026-03-14 10:27:51 +0700

    Update ExamPortal.tsx

--- components/ExamPortal.tsx
+++ components/ExamPortal.tsx
@@ -267,7 +267,7 @@ const ExamPortal: React.FC<ExamPortalProps> = ({ grade: rawGrade, onBack, onStar
     <div className="relative">
       <select className="w-full p-4 md:p-5 min-h-[44px] bg-slate-50 border-2 border-slate-100 rounded-2xl md:rounded-3xl font-black text-blue-800 focus:ring-4 focus:ring-blue-100 shadow-sm outline-none appearance-none" value={selectedCode} onChange={e => setSelectedCode(e.target.value)}>
         <option value="">-- CHỌN MÃ ĐỀ --</option>
-        {allAvailableCodes.map(c => <option key={c.code} value={c.code}>{c.name}</option>)}
+        {allAvailableCodes.map(c => <option key={c.code} value={c.code}>{c.code} - {c.name}</option>)}
       </select>
       <i className="fas fa-chevron-down absolute right-6 top-1/2 -translate-y-1/2 text-blue-400 pointer-events-none"></i>
     </div>
