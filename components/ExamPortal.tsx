3bb051140f5ff6082f1d4c1106fef27d46c44b03
vietnamexam <vanhayd2@gmail.com>
2026-03-14 11:14:18 +0700

    Update ExamPortal.tsx

--- components/ExamPortal.tsx
+++ components/ExamPortal.tsx
@@ -130,7 +130,7 @@ const currentCodeDef = useMemo(() =>
     if (!fc) return alert("Cấu hình đề thi bị lỗi!");
 
     const finalConfig = { 
-      String(selectedCode).replace(/'/g, "").trim(),
+      id: String(selectedCode).replace(/'/g, "").trim(),
       title: currentCodeDef.name, 
       time: fc.duration, 
       mcqPoints: fc.scoreMC, tfPoints: fc.scoreTF, saPoints: fc.scoreSA, 
