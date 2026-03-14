d2b1de3e8ab758cf2a1a22bc4d5dea698fb56be7
vietnamexam <vanhayd2@gmail.com>
2026-03-14 08:15:32 +0700

    Add files via upload

--- components/Watermark.tsx
+++ components/Watermark.tsx
@@ -23,7 +23,7 @@ const Watermark: React.FC<WatermarkProps> = ({ text, seed = 0 }) => {
   const watermarkItems = useMemo(() => {
 
     const items = [];
-    const count = 10;
+    const count = 6;
 
     for (let i = 0; i < count; i++) {
 
