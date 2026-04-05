31b6260972dd62ae449293f8ab1bd9bf88244780
vietnamexam <vanhayd2@gmail.com>
2026-04-05 21:54:01 +0700

    Update ExamPortal.tsx

--- components/ExamPortal.tsx
+++ components/ExamPortal.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useMemo, useEffect } from 'react';
 import { Question, Student, ExamCodeDefinition } from '../types';
-import { KETQUA_URL, TOPICS_DATA, EXAM_CODES } from '../config';
+import { TOPICS_DATA, EXAM_CODES, handle_chonmon } from '../config';
 import { pickQuestionsSmart } from '../questions';
 
 interface ExamPortalProps {
